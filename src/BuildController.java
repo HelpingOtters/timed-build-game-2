fd239e00ea1158c9d40ee1b1e3d50a94f0f74a75
thedandano <dandano2006@gmail.com>
2019-12-01 17:07:48 -0800

    Fixed doNothing method
    Changed 100 to 1000 milliseconds

--- src/BuildController.java
+++ src/BuildController.java
@@ -419,7 +419,7 @@ public class BuildController implements ActionListener {
                 }
                 // JLabel text
                 setText(timeFormat(sec));
-                doNothing(100); //100 millisecond pause 
+                doNothing(1000); //100 millisecond pause 
              }
           }
  
